1bc5cc2df6843e799e0ba3a334debcc22a44c68b
sunnysideup <sunnysideupdevs@gmail.com>
2016-11-29 14:02:47 +1300

    PATCH: module clean-up

--- LatestBlogEntries.php
+++ LatestBlogEntries.php
@@ -5,34 +5,35 @@
  *
  * @package blog
  */
-class LatestBlogEntries extends Widget {
+class LatestBlogEntries extends Widget
+{
+    private static $db = array(
+        'NumberOfItems' => 'Int',
+        'OnlyFromThisPage' => 'Boolean'
+    );
+    private static $defaults = array(
+        'NumberOfItems' => 7
+    );
 
-	private static $db = array(
-		'NumberOfItems' => 'Int',
-		'OnlyFromThisPage' => 'Boolean'
-	);
-	private static $defaults = array(
-		'NumberOfItems' => 7
-	);
+    private static $title = 'Latest Blog Entries';
 
-	private static $title = 'Latest Blog Entries';
+    private static $cmsTitle = 'Latest Blog Entries';
 
-	private static $cmsTitle = 'Latest Blog Entries';
+    private static $description = 'Show a list of latest blog entries.';
 
-	private static $description = 'Show a list of latest blog entries.';
+    public function getCMSFields()
+    {
+        return new FieldList(
+            new NumericField("NumberOfItems", "Number Of Items Shown")
+        );
+    }
 
-	function getCMSFields() {
-		return new FieldList(
-			new NumericField("NumberOfItems","Number Of Items Shown")
-		);
-	}
-
-	public function WidgetBlogEntries() {
-		if(!$this->NumberOfItems) {
-			$this->NumberOfItems = 1;
-		}
-		Requirements::themedCSS("widgets_latestblogentries", "widgets_latestblogentries");
-		return BlogPost::get()->filter(array("ShowInSearch" => 1))->sort(array("Created" => "DESC"))->limit($this->NumberOfItems);
-	}
-
+    public function WidgetBlogEntries()
+    {
+        if (!$this->NumberOfItems) {
+            $this->NumberOfItems = 1;
+        }
+        Requirements::themedCSS("widgets_latestblogentries", "widgets_latestblogentries");
+        return BlogPost::get()->filter(array("ShowInSearch" => 1))->sort(array("Created" => "DESC"))->limit($this->NumberOfItems);
+    }
 }
